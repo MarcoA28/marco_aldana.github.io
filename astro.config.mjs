a2cd1d605facb85cb905e6368fdcd5aef47e0df3
Marco Aldana <131915125+MarcoA28@users.noreply.github.com>
2026-01-08 15:04:48 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MarcoA28.github.io',
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
